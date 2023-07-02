98af5ab0e6b0b9249acdf0f82fc30dfc07010494
Autómata Central <ingeniero@Mac-mini-de-Automata.local>
2023-07-02 00:54:59 -0500

    ecmascrip 7

---
+++ src/es7/02-array-includes.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.includes(4));
+
+// se debe observar que los valores utilicen mayúsculas y minúsculas en strings. También cuando se manejen números dentro del string, se tendrían que convertir en valor númerico para luego comparar
+const list = ["Lotus", "Audi", "Lexus"];
+
+console.log(list.includes("Lotus"));
